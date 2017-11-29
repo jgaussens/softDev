2c2c6fca2e476e5c2d5a852be04de40a53713dc4
ArthurThomasset <arthur@thomasset.fr>
2017-11-29 15:50:17 +0100

    correction des bugs

--- src/main/java/Dryad.java
+++ src/main/java/Dryad.java
@@ -7,7 +7,7 @@ public class Dryad extends Card {
     public static final String NAME = "Dryad";
 
     //fonction à finir
-    public void power(Gamer gamer1, Gamer gamer2){
+   /* public void power(Gamer gamer1, Gamer gamer2){
         //Comment choper l'indice de cette carte (self ça existe pas en C#)
     	int i = 0;
     	while(gamer1.getHandCards().get(i) != "Dryad") // faire un toString ici pour choper là où c'est une driade
@@ -18,7 +18,7 @@ public class Dryad extends Card {
     	//Choper le pouvoir du même indice sur le board d'en face
     		// que faire si personne en face?
     		//doit-on override la fonction pouvoir ?
-     }
+     }*/
 
     public String toString()
     {
