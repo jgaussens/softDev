74d0563430f73e393c83e0cee3a240738da55b73
LudovicRansau <ludovic.ransau@gmail.com>
2017-12-15 10:50:34 +0100

    Update Dryad.java
    Now it's dynamize !

--- src/main/java/ch/makery/address/model/Dryad.java
+++ src/main/java/ch/makery/address/model/Dryad.java
@@ -8,21 +8,25 @@ public class Dryad extends Card {
     //fonction à finir
     public void power(Gamer gamer, Gamer opponent)
     {
-        System.out.println("Dryad's power activated.");
-      /*  //Comment choper l'indice de cette carte (self ça existe pas en C#)
+       System.out.println("Dryad's power activated.");
+      /*  //Comment choper l'indice de cette carte (self Ã§a existe pas en C#)
     	int i = 0;
-    	while(gamer1.getHandCards().get(i).toString() != "Dryad") // faire un toString ici pour choper là où c'est une driade
+    	while(gamer1.getHandCards().get(i).toString() != "Dryad") // faire un toString ici pour choper lÃ  oÃ¹ c'est une driade
     	{
     		i+=1;
     		
     	}
-    	//Choper le pouvoir du même indice sur le board d'en face
+    	//Choper le pouvoir du mÃªme indice sur le board d'en face
     		// que faire si personne en face?
     		//doit-on override la fonction pouvoir ?
     */
 
         //@todo: dynamize in FRONT, get the index of the card the user wants to steal
-        int indexToSteal = 1;
+        //@done
+        int value;
+		Random rand = new Random();
+		value = rand.nextInt(opponent.getHandCards().size());
+        int indexToSteal = value;
 
         gamer.getBoardCards().add(opponent.getBoardCards().get(indexToSteal));
         opponent.getBoardCards().remove(indexToSteal);
