c9777d05b5c61029056d7871510c4b20636a26fc
Chloé Mezouar <32333272+cmezouar@users.noreply.github.com>
2017-12-20 10:39:16 +0100

    Update DryadTest.java

--- test/main/java/DryadTest.java
+++ test/main/java/DryadTest.java
@@ -1,12 +1,34 @@
-package main.java;
-
+package model;
+import ch.makery.address.model.*;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class DryadTest {
+
+    Elf elf = new Elf();
+    Dryad dryad= new Dryad();
+
     @Test
     public void power() throws Exception {
+
+        Gamer g1=new Gamer();
+        Gamer g2= new Gamer();
+
+
+        g2.getBoardCards().add(elf);
+        int n1before=g1.getBoardCards().size();
+        int n2before=g2.getBoardCards().size();
+
+        dryad.power(g1,g2);
+
+
+        int n1after=g1.getBoardCards().size();
+        int n2after=g2.getBoardCards().size();
+
+        assertEquals(n1after,n1before+1);
+        assertEquals(n2after,n2before-1);
+
     }
 
 }
