37e37d357e5b466760cc4855288c1fa2907fba51
Chloé Mezouar <32333272+cmezouar@users.noreply.github.com>
2017-12-20 10:41:48 +0100

    Update GoblinTest.java

--- test/main/java/GoblinTest.java
+++ test/main/java/GoblinTest.java
@@ -1,12 +1,49 @@
-package main.java;
-
+package model;
+import ch.makery.address.model.*;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class GoblinTest {
+
+
+    Elf elf= new Elf();
+    Gnome gnome= new Gnome();
+    Dryad dryad =new Dryad();
+
+    Korrigan korrigan=new Korrigan();
+
+    Goblin goblin =new Goblin();
     @Test
     public void power() throws Exception {
+        Gamer g1= new Gamer();
+        Gamer g2= new Gamer();
+        Gamer g3=new Gamer();
+        Gamer g4=new Gamer();
+
+        g1.getHandCards().add(elf);
+        g1.getHandCards().add(gnome);
+
+        g2.getHandCards().add(dryad);
+        g2.getHandCards().add(korrigan);
+
+        goblin.power(g1,g2);
+
+        g3.getHandCards().add(elf);
+        g3.getHandCards().add(gnome);
+
+        g4.getHandCards().add(dryad);
+        g4.getHandCards().add(korrigan);
+
+        assertEquals(g1.getHandCards(),g4.getHandCards());
+        assertEquals(g2.getHandCards(),g3.getHandCards());
+
+
+
+
+
+
+
     }
 
 }
